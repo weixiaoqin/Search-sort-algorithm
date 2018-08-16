83d179e15a93ee0962096103021fdb1eb78b3ac1
weixiaoqin <weixiaoqin94@126.com>
2018-08-16 14:55:53 +0800

    N9

--- src/剑指offer/N9替换空格.java
+++ src/剑指offer/N9替换空格.java
@@ -1,6 +1,7 @@
 package 剑指offer;
 
-<<<<<<< HEAD
+//请实现一个函数，将一个字符串中的每个空格替换成“%20”。例如，当字符串为We Are Happy.
+//则经过替换之后的字符串为We%20Are%20Happy。
 public class N9替换空格  {
 	
 	    public String replaceSpace(StringBuffer str) {
@@ -19,8 +20,4 @@ public class N9替换空格  {
 }
   
 
-=======
-public class N9替换空格 {
-  public String 
-}
->>>>>>> branch 'master' of https://github.com/weixiaoqin/offer.git
+
