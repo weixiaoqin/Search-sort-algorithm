40f4feb232e69491be9048947847d706ebe3e3be
weixiaoqin <weixiaoqin94@126.com>
2018-09-22 21:51:58 +0800

    2

--- src/华为/H19删除字符串中出现次数最少的字符.java
+++ src/华为/H19删除字符串中出现次数最少的字符.java
@@ -14,39 +14,44 @@ import java.util.Scanner;
 //abcdd
 //dd
 public class H19删除字符串中出现次数最少的字符 {
- public static void main(String[] args) {
-	Scanner sc=new Scanner(System.in);
-	while(sc.hasNext()) {
-		String s=sc.nextLine();
-		char [] a=s.toCharArray();
-		
-		Map<Character, Integer> map=new LinkedHashMap<>();
-		//LinkedHashMap 是HashMap的一个子类，保存了记录的插入顺序，
-		//在用Iterator遍历LinkedHashMap时，先得到的记录肯定是先插入的
-		int min=a.length;
-		char chmin=a[0];
-		for(int i=0;i<a.length;i++) {
-			if(map.containsKey(a[i])) {
-				map.put(a[i], map.get(a[i])+1);
-			}
-			else {
-				map.put(a[i], 1);
-			} 
-			
-			if(map.get(a[i])<min) {
-				min=map.get(a[i]);
-				chmin=a[i];
+	 public static void main(String[] args) {
+			Scanner sc=new Scanner(System.in);
+			while(sc.hasNext()) {
+				String s=sc.nextLine();
+				char [] a=s.toCharArray();
+				
+				Map<Character, Integer> map=new LinkedHashMap<>();
+				//LinkedHashMap 是HashMap的一个子类，保存了记录的插入顺序，
+				//在用Iterator遍历LinkedHashMap时，先得到的记录肯定是先插入的
+				int min=a.length;
+				char chmin=a[0];
+				for(int i=0;i<a.length;i++) {
+					if(map.containsKey(a[i])) {
+						map.put(a[i], map.get(a[i])+1);
+					}
+					else {
+						map.put(a[i], 1);
+					} 
+					
+					
+				}
+		        for(char a1:a){
+		            if(map.get(a1)<min) {
+						min=map.get(a1);
+						chmin=a1;
+					}
+		        }
+				StringBuilder sb=new StringBuilder("");
+				for(int i=0;i<a.length;i++) {
+//					if(a[i]!=chmin) {
+//						sb.append(a[i]);
+//					}
+		              if(map.get(a[i])!=min)
+		                sb.append(a[i]);
+				}
+				
+				
+				System.out.println(sb.toString());
 			}
 		}
-		StringBuilder sb=new StringBuilder("");
-		for(int i=0;i<a.length;i++) {
-			if(a[i]!=chmin) {
-				sb.append(a[i]);
-			}
-		}
-		
-		
-		System.out.println(sb.toString());
-	}
-}
 }
